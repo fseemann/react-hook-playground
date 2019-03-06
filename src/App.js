48fbd6e3299f7799e9163b2bf5e4a8c14b9dea22
fseemann <fseemann@mail.de>
2019-03-06 18:32:03 +0100

    Add formik form variation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { useForm, ifTrueAppend } from './forms'
+import {Formik, Form, Field} from 'formik'
 import './App.css'
 import '../node_modules/semantic-ui-css/semantic.min.css'
 import * as yup from 'yup'
 
-export default function App() {
-  const shape = yup.object().shape({
-    name: yup.string().email('Name must be a valid email.').required('Name is required.').default('test@test.de'),
-    password: yup.string().required('Password is required')
-  });
+function NativeForm() {
+  const shape = formSchema();
 
   const onSubmit = (state, setErrors) => { 
     console.log('submit', state); 
@@ -30,11 +28,61 @@ export default function App() {
         </div>
       </div>
       <button className="ui fluid primary button">Login</button>
-      <div className="ui error message">
-        <ul className="list">
-          {Object.keys(errors).flatMap(it => errors[it]).map(it => <li key={it}>{it}</li>)}
-        </ul>
-      </div>
+      <SemanticErrorList errors={errors}/>
     </form>
   )
 }
+
+function FormikForm() {
+  const onSubmit = (values, actions) => {
+    console.log('submit', values);
+    actions.setErrors({
+      'password': 'Invalid password.'
+    })
+  }
+
+  return <Formik
+    initialValues={{
+      name: '',
+      password: ''
+    }}
+    onSubmit={onSubmit}
+    validationSchema={formSchema()}
+  >
+    {({ errors }) => (
+      <Form className={'ui large form' + ifTrueAppend(Object.keys(errors).length > 0, 'error')}>
+        <Field name="name" placeholder="Username" component={SemanticField} />
+        <Field name="password" type="password" placeholder="Password" component={SemanticField} />
+        <button type="submit" className="ui fluid primary button">Login</button>
+        <SemanticErrorList errors={errors} />
+      </Form>
+    )}
+  </Formik>
+}
+
+function SemanticField({field, form: {errors}, ...props}) {
+  return (
+  <div className={'field' + ifTrueAppend(errors[field.name], 'error')}>
+    <div className="ui input">
+      <input {...field} {...props} ></input>
+    </div>
+  </div>
+  )
+}
+
+const SemanticErrorList = (props) => (
+  <div className="ui error message">
+    <ul className="list">
+      {Object.keys(props.errors).flatMap(it => props.errors[it]).map((it, index) => <li key={index}>{it}</li>)}
+    </ul>
+  </div>
+)
+
+function formSchema() {
+  return yup.object().shape({
+      name: yup.string().email('Name must be a valid email.').required('Name is required.'),
+      password: yup.string().required('Password is required')
+    });
+  }
+  
+export { NativeForm, FormikForm }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {NativeForm, FormikForm} from './App';
 import * as serviceWorker from './serviceWorker';
+
+function App() {
+    return <div>
+        <NativeForm></NativeForm>
+        <FormikForm></FormikForm>
+    </div>
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
